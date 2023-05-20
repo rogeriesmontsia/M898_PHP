3de3ab7411e81f620f03b68e5b20f3a2076ab2e4
rogeriesmontsia <118528868+rogeriesmontsia@users.noreply.github.com>
2023-05-20 23:41:09 +0000

    aaa

--- dashboard.php
+++ dashboard.php
@@ -80,9 +80,9 @@ $username = $_SESSION['username'];
 </header>
 
 <div class="container-fluid">
-  <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="position-sticky pt-3">
+    <div class="row">
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+             <div class="position-sticky pt-3">
         <ul class="nav flex-column">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">
@@ -129,10 +129,8 @@ $username = $_SESSION['username'];
 
     
       
-<div class="container">
-  <div class="row">
-    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-      <h2>Tauler d'incidències</h2>
+    <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <h2>Tauler d'incidències</h2>
 
       <?php
       require_once 'conexiodb.php';
