aaa174c33c88b11969040d04d7195b7afe46e502
rogeriesmontsia <118528868+rogeriesmontsia@users.noreply.github.com>
2023-05-21 09:17:39 +0000

    aa

--- inserir_usuaris.php
+++ inserir_usuaris.php
@@ -132,7 +132,7 @@ $username = $_SESSION['username'];
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         
 
-      <h2>Section title</h2>
+     
 <div class="container">
     <div class="row">
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
--- material.php
+++ material.php
@@ -131,7 +131,7 @@ $username = $_SESSION['username'];
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         
 
-      <h2>Section title</h2>
+      
       
 <div class="container">
   <div class="row">
